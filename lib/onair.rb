d2967b329c321abeca0aca261847302260e1b416
Dan Miller <dan@dmiller.dev>
2020-08-25 17:57:12 -0400

    remember the previous setting

--- lib/onair.rb
+++ lib/onair.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'sinatra/json'
 require 'http'
+require 'pp'
+
+# TODO(dmiller): make sure that env variables are set
 
 LIVING_ROOM_LIGHT = 9
 BEDROOM_LIGHT = 10
@@ -9,12 +12,14 @@ ON_AIR_COLOR = {bri: 254, hue: 46563, sat: 213}
 OFF_AIR_COLOR = {bri: 254, hue: 52349, sat: 21}
 # TODO(dmiller): more states
 
+# map of light ID to last known state that was not set by onair
+$last_known_manual_state = {}
+
 def light_is(status, color)
-    puts status
     status[:bri] == color[:bri] && status[:hue] == color[:hue] && status[:sat] == color[:sat]
 end
 
-def light_status(status)
+def on_or_off_air(status)
     if light_is(status, ON_AIR_COLOR)
         "ON_AIR"
     else
@@ -29,25 +34,33 @@ def get_light_status(id)
     user_id = ENV["PHILLIPS_HUE_USER_ID"]
     bridge_ip = ENV["PHILLIPS_HUE_BRIDGE_IP"]
 
-    resp = HTTP.get("http://#{bridge_ip}/api/#{user_id}/lights/#{id}")
+    url = "http://#{bridge_ip}/api/#{user_id}/lights/#{id}"
+    resp = HTTP.get(url)
     if !resp.status.success?
         puts "Error getting light status"
         puts resp
         return
     end
 
-    js = resp.parse
+    resp.parse
+end
 
-    light_status(js)
+def status_set_by_onair(status)
+    s = status["state"]
+    light_is(s, ON_AIR_COLOR) || light_is(s, OFF_AIR_COLOR)
 end
 
 def set_light(id, color)
-    # TODO(dmiller): should be possible to init this on start?
     user_id = ENV["PHILLIPS_HUE_USER_ID"]
     bridge_ip = ENV["PHILLIPS_HUE_BRIDGE_IP"]
+    allowed_keys = ["on", "bri", "hue", "sat"]
 
-    payload = color
-    payload[:on] = true
+    payload = color.select { |key, _| allowed_keys.include?(key)}
+    if !payload.key?("on")
+        payload["on"] = true
+    end
+
+    pp payload
 
     resp = HTTP.put("http://#{bridge_ip}/api/#{user_id}/lights/#{id}/state", :json => payload)
     if !resp.status.success?
@@ -64,9 +77,23 @@ get '/status' do
 end
 
 get '/light/:light/onair' do
+    id = params['light']
+    current_status = get_light_status(id)
+    if !status_set_by_onair(current_status)
+        puts "We didn't set the current status, so let's store it"
+        $last_known_manual_state[id] = current_status
+    end
+
     set_light(params['light'], ON_AIR_COLOR)
 end
 
 get '/light/:light/offair' do
-    set_light(params['light'], OFF_AIR_COLOR)
+    light = params['light']
+    color = OFF_AIR_COLOR
+    if $last_known_manual_state.key?(light) 
+        color = $last_known_manual_state[light]["state"]
+        pp color
+        $last_known_manual_state.delete(light)
+    end
+    set_light(params['light'], color)
 end
